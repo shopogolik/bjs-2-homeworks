dcb8b884ac74aeb1e954dfaa967a6d707615d8b5
shopogolik <shopo@ya.ru>
2022-02-07 12:31:33 +0100

    changed task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,20 @@
+"use strict";
 function solveEquation(a, b, c) {
-    "use strict";
-
     let arr = [];
     let d = b * b - 4 * a * c;
     let x1, x2;
-    arr.push (x1, x2)
+
     if (d > 0) {
         x1 = (-b + Math.sqrt(d)) / (2 * a),
         x2 = (-b - Math.sqrt(d)) / (2 * a);
-        return arr;
-        // array
+
     } else if (d == 0) {
         x1 = -b / (2 * a);
         return arr;
         // array
-    } else if (d < 0)
-        return arr;
-    // array
+    }
 
+    arr.push(x1, x2);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
