081d713fc72ef766a7fabe277c7a91300833382b
shopogolik <shopo@ya.ru>
2022-02-19 21:24:53 +0100

    task1 again changed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,44 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+    let min, max, sum, avg;
+    min = Infinity;
+    max = -Infinity;
+    let sumSub;
+    for (let i = 0; i < arr.length; i++) {
+        if (min > [i - 1]) {
+            min = [i];
+        }
+        if (max > [i - 1]) {
+            max = [i];
+        }
+        sumSub = sum + [i];
+    }
+    avg = Number((sumSub / arr.length).toFixed(2));
+    return {
+        min: min,
+        max: max,
+        avg: avg
+    };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum;
 
-  // Ваш код
+    // Ваш код
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
-function worker2(arr) {
-  // Ваш код
+function worker2(arr) {// Ваш код
 }
